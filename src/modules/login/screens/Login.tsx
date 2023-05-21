2a260a4218cb30b307bf11b5f66ba405239e5be2
matheusnerisxavier <matheus.nerisxa@outlook.com>
2023-05-21 16:17:24 -0300

    text customizado

--- src/modules/login/screens/Login.tsx
+++ src/modules/login/screens/Login.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
 
 import Button from '../../../shared/components/button/Button';
 import Input from '../../../shared/components/input/input';
@@ -12,7 +12,6 @@ const Login = () => {
   return (
     <View>
       <ContainerLogin>
-        <Text>Matheus</Text>
         <Input />
         <Button margin="16px" title="ENTRAR" onPress={handleOnPress} />
       </ContainerLogin>
