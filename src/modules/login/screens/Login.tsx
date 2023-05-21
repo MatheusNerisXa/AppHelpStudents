c2bce2186c06501f13a27c4c4ddbe21c741a9bf0
matheusnerisxavier <matheus.nerisxa@outlook.com>
2023-05-21 13:02:49 -0300

    criando botao personalizado

--- src/modules/login/screens/Login.tsx
+++ src/modules/login/screens/Login.tsx
@@ -1,14 +1,20 @@
 import { Text, View } from 'react-native';
 
+import Button from '../../../shared/button/Button';
 import Input from '../../../shared/components/input/input';
 import { ContainerLogin } from '../styles/login.style';
 
 const Login = () => {
+  const handleOnPress = () => {
+    console.log('Clicou');
+  };
+
   return (
     <View>
       <ContainerLogin>
         <Text>Matheus</Text>
         <Input />
+        <Button margin="16px" title="ENTRAR" onPress={handleOnPress} />
       </ContainerLogin>
     </View>
   );
