b127685d986809f2639b1c36a5d393cbed1520e1
matheusnerisxavier <matheus.nerisxa@outlook.com>
2023-05-21 19:37:56 -0300

    personalizando os tipos dos botoes

--- src/shared/components/text/text.style.ts
+++ src/shared/components/text/text.style.ts
@@ -3,10 +3,11 @@ import styled from 'styled-components/native';
 interface ContainerTextProps {
   color?: string;
   fontSize: string;
+  fontFamily: 'Poppins-Bold' | 'Poppins-Light' | 'Poppins-Regular';
 }
 export const ContainerText = styled.Text<ContainerTextProps>`
   ${(props: { color: unknown }) => (props.color ? `color: ${props.color}` : '')};
 
-  font-family: Poppins-Regular;
+  font-family: ${(props: { fontFamily: unknown }) => props.fontFamily};
   font-size: ${(props: { fontSize: unknown }) => props.fontSize};
 `;
