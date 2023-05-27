9205b48734ba527c0a9689bde51e4a3b6417beac
matheusnerisxavier <matheus.nerisxa@outlook.com>
2023-05-27 14:29:06 -0300

    logo

--- src/modules/login/screens/Login.tsx
+++ src/modules/login/screens/Login.tsx
@@ -3,7 +3,7 @@ import { View } from 'react-native';
 import Button from '../../../shared/components/button/Button';
 import Input from '../../../shared/components/input/input';
 import { theme } from '../../../shared/themes/theme';
-import { ContainerLogin } from '../styles/login.style';
+import { ContainerLogin, Imagelogo } from '../styles/login.style';
 
 const Login = () => {
   const handleOnPress = () => {
@@ -13,6 +13,7 @@ const Login = () => {
   return (
     <View>
       <ContainerLogin>
+        <Imagelogo resizeMode="cover" source={require('../../../assets/images/logo.png')} />
         <Input margin="0px 0px 8px 0px" placeholder="Digite seu email" title="Email:" />
         <Input secureTextEntry placeholder="Digite sua senha" title="Senha:" />
         <Button
--- src/modules/login/styles/login.style.ts
+++ src/modules/login/styles/login.style.ts
@@ -10,3 +10,9 @@ export const ContainerLogin = styled.View`
   align-items: center;
   padding: 16px;
 `;
+
+export const Imagelogo = styled.Image`
+  width: 400px;
+  height: 200px;
+  margin-bottom: 24px;
+`;
