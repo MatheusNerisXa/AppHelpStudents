f7f143b11070120528fe0cf401c6d70c14d24459
matheusnerisxavier <matheus.nerisxa@outlook.com>
2023-05-27 11:05:54 -0300

    ajuste input

--- src/shared/components/text/text.style.ts
+++ src/shared/components/text/text.style.ts
@@ -3,10 +3,13 @@ import styled from 'styled-components/native';
 interface ContainerTextProps {
   color?: string;
   fontSize: string;
-  fontFamily: 'Poppins-Bold' | 'Poppins-Light' | 'Poppins-Regular';
+  customMargin?: string;
+  fontFamily: 'Poppins-Bold' | 'Poppins-Light' | 'Poppins-Regular' | 'Poppins-SemiBold';
 }
 export const ContainerText = styled.Text<ContainerTextProps>`
-  ${(props: { color: unknown }) => (props.color ? `color: ${props.color}` : '')};
+  ${(props: { color: unknown }) => (props.color ? `color: ${props.color};` : '')};
+  ${(props: { customMargin: unknown }) =>
+    props.customMargin ? `margin: ${props.customMargin};` : ''};
 
   padding-top: 3px;
   font-family: ${(props: { fontFamily: unknown }) => props.fontFamily};
