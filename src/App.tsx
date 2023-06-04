dd53c5cae1bc13d143522e9a7a0c3fb83959b3a7
matheusnerisxavier <matheus.nerisxa@outlook.com>
2023-06-04 12:30:39 -0300

    navegacao

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
-import { SafeAreaView } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from 'react';
 import { Provider } from 'react-redux';
 
+import Home from './modules/home';
 import Login from './modules/login';
 import GlobalModal from './shared/components/modal/globalModal/GlobalModal';
 import store from './store';
 
+const Stack = createNativeStackNavigator();
+
 const App = () => {
   return (
     <Provider store={store}>
-      <SafeAreaView>
-        <GlobalModal />
-        <Login />
-      </SafeAreaView>
+      <GlobalModal />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="Home" component={Home} options={{ title: 'Home' }} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </Provider>
   );
 };
