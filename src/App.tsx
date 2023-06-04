b3ec6913d09637e91f74ee643e0743298e8261a0
matheusnerisxavier <matheus.nerisxa@outlook.com>
2023-06-04 11:48:02 -0300

    modal para todas as telad

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { SafeAreaView } from 'react-native';
 import { Provider } from 'react-redux';
 
 import Login from './modules/login';
+import GlobalModal from './shared/components/modal/globalModal/GlobalModal';
 import store from './store';
 
 const App = () => {
   return (
     <Provider store={store}>
       <SafeAreaView>
+        <GlobalModal />
         <Login />
       </SafeAreaView>
     </Provider>
