370c3de2579c50d0feff1459bd05e79be4273b56
matheusnerisxavier <matheus.nerisxa@outlook.com>
2023-06-04 13:32:34 -0300

    ajuste nas rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,15 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
 import { Provider } from 'react-redux';
 
-import Home from './modules/home';
-import Login from './modules/login';
+import Navigation from './Navigation';
 import GlobalModal from './shared/components/modal/globalModal/GlobalModal';
 import store from './store';
-
-const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
     <Provider store={store}>
       <GlobalModal />
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Login" component={Login} />
-          <Stack.Screen name="Home" component={Home} options={{ title: 'Home' }} />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <Navigation />
     </Provider>
   );
 };
