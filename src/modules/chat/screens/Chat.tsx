d0f44001975b4e325307ec6381d745e45b40680f
matheusnerisxavier <matheus.nerisxa@outlook.com>
2023-07-23 10:55:27 -0300

    Revert "corrigindo problema da chave do chat"
    This reverts commit aeb4af1ac6c1af7d98c4c74b4b76a1de3ff1ef8e.

--- src/modules/chat/screens/Chat.tsx
+++ src/modules/chat/screens/Chat.tsx
@@ -4,16 +4,14 @@ import {
   KeyboardAvoidingView,
   Platform,
   ScrollView,
+  StyleSheet,
   Text,
   TextInput,
   TouchableOpacity,
   View,
 } from 'react-native';
 
-import { KEY_CHAT } from '../../../../config'; // Importe a chave de API do arquivo config.js
-import chatStyle from '../styles/chat.style';
-
-const OPENAI_API_KEY = KEY_CHAT;
+const OPENAI_API_KEY = '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';
 
 const Chat = () => {
   const [inputText, setInputText] = useState('');
@@ -65,43 +63,98 @@ const Chat = () => {
 
   return (
     <KeyboardAvoidingView
-      style={chatStyle.container}
+      style={styles.container}
       behavior={Platform.OS === 'ios' ? 'padding' : null}
       keyboardVerticalOffset={Platform.OS === 'ios' ? 100 : 0} // Ajuste o valor conforme necessário
     >
       <ScrollView
-        style={chatStyle.chatContainer}
+        style={styles.chatContainer}
         ref={scrollViewRef}
         onContentSizeChange={() => scrollViewRef.current.scrollToEnd({ animated: true })}
       >
         {chatLog.map((message, index) => (
           <View key={index}>
             {message.type === 'user' && (
-              <View style={chatStyle.userMessage}>
-                <Text style={chatStyle.messageText}>{message.text}</Text>
+              <View style={styles.userMessage}>
+                <Text style={styles.messageText}>{message.text}</Text>
               </View>
             )}
             {message.type === 'ai' && (
-              <View style={chatStyle.aiMessage}>
-                <Text style={chatStyle.messageText}>{message.text}</Text>
+              <View style={styles.aiMessage}>
+                <Text style={styles.messageText}>{message.text}</Text>
               </View>
             )}
           </View>
         ))}
       </ScrollView>
-      <View style={chatStyle.inputContainer}>
+      <View style={styles.inputContainer}>
         <TextInput
-          style={chatStyle.input}
+          style={styles.input}
           onChangeText={setInputText}
           value={inputText}
           placeholder="Digite sua mensagem..."
         />
-        <TouchableOpacity style={chatStyle.sendButton} onPress={sendMessage}>
-          <Text style={chatStyle.sendButtonText}>Enviar</Text>
+        <TouchableOpacity style={styles.sendButton} onPress={sendMessage}>
+          <Text style={styles.sendButtonText}>Enviar</Text>
         </TouchableOpacity>
       </View>
     </KeyboardAvoidingView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F5F5F5',
+  },
+  chatContainer: {
+    flex: 1,
+    padding: 10,
+  },
+  userMessage: {
+    backgroundColor: '#64B5F6',
+    alignSelf: 'flex-start',
+    borderRadius: 10,
+    marginBottom: 10,
+    padding: 10,
+    maxWidth: '70%',
+  },
+  aiMessage: {
+    backgroundColor: '#81C784',
+    alignSelf: 'flex-end',
+    borderRadius: 10,
+    marginBottom: 10,
+    padding: 10,
+    maxWidth: '70%',
+  },
+  messageText: {
+    color: '#FFF',
+    fontSize: 16,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderTopWidth: 1,
+    borderTopColor: '#CCC',
+    padding: 10,
+  },
+  input: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: '#CCC',
+    borderRadius: 5,
+    padding: 10,
+    marginRight: 10,
+  },
+  sendButton: {
+    backgroundColor: '#2196F3',
+    padding: 10,
+    borderRadius: 5,
+  },
+  sendButtonText: {
+    color: '#FFF',
+    fontWeight: 'bold',
+  },
+});
 
 export default Chat;
