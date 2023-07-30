1e9c8aceeed0ecc84801ddf61e3a3b894abddf6f
matheusnerisxavier <matheus.nerisxa@outlook.com>
2023-07-30 08:48:58 -0300

    melhorias na tela home

--- src/modules/home/screens/Home.tsx
+++ src/modules/home/screens/Home.tsx
@@ -1,6 +1,7 @@
 import { NavigationProp, ParamListBase, useNavigation } from '@react-navigation/native';
-import React from 'react';
-import { ScrollView, TouchableOpacity, View } from 'react-native';
+import React, { useEffect, useRef } from 'react';
+import { Image, ScrollView, TouchableOpacity, View } from 'react-native';
+import * as Animatable from 'react-native-animatable';
 
 import { Icon } from '../../../shared/components/icon/Icon';
 import Text from '../../../shared/components/text/Text';
@@ -12,7 +13,6 @@ const Home = () => {
   const navigation = useNavigation<NavigationProp<ParamListBase>>();
 
   const { user } = useRequest();
-
   const userName = user?.name;
 
   const handleExamPress = () => {
@@ -23,7 +23,7 @@ const Home = () => {
     navigation.navigate(MenuUrl.News);
   };
 
-  const handleSuportPress = () => {
+  const handleSupportPress = () => {
     navigation.navigate(MenuUrl.Suport);
   };
 
@@ -31,45 +31,77 @@ const Home = () => {
     navigation.navigate(MenuUrl.Translation);
   };
 
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const bannerRef = useRef<Animatable.View | number | undefined | any>(null);
+
+  useEffect(() => {
+    if (bannerRef.current) {
+      bannerRef.current.bounceIn(1500);
+    }
+  }, []);
+
   return (
     <ScrollView contentContainerStyle={homeStyle.container}>
-      <View style={homeStyle.greetingContainer}>
+      <View style={homeStyle.headerContainer}>
         <Text style={homeStyle.greetingText}>Olá,</Text>
-        <View style={homeStyle.userNameContainer}>
-          <Text style={homeStyle.userName}>{userName}!</Text>
-        </View>
+        <Text style={homeStyle.userName}>{userName}!</Text>
       </View>
 
-      <View style={homeStyle.cardsContainer}>
-        <View style={homeStyle.cardRow}>
-          <TouchableOpacity style={homeStyle.cardContainer} onPress={handleExamPress}>
-            <View style={homeStyle.iconContainer}>
-              <Icon name="books" size={24} color="#007AFF" style={homeStyle.icon} />
-            </View>
+      <View style={homeStyle.cardRow}>
+        <TouchableOpacity onPress={handleExamPress}>
+          <Animatable.View animation="fadeInLeft" duration={1000} style={homeStyle.cardContainer}>
+            <Icon name="books" size={32} color="#007AFF" style={homeStyle.icon} />
             <Text style={homeStyle.cardText}>Vestibulares</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={homeStyle.cardContainer} onPress={handleNewsPress}>
-            <View style={homeStyle.iconContainer}>
-              <Icon name="newspaper" size={24} color="#007AFF" style={homeStyle.icon} />
-            </View>
+          </Animatable.View>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={handleNewsPress}>
+          <Animatable.View animation="fadeInRight" duration={1000} style={homeStyle.cardContainer}>
+            <Icon name="newspaper" size={32} color="#007AFF" style={homeStyle.icon} />
             <Text style={homeStyle.cardText}>Notícias</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={homeStyle.cardContainer} onPress={handleSuportPress}>
-            <View style={homeStyle.iconContainer}>
-              <Icon name="question" size={24} color="#007AFF" style={homeStyle.icon} />
-            </View>
-            <Text style={homeStyle.cardText}>Ajuda</Text>
-          </TouchableOpacity>
-        </View>
-        <View style={homeStyle.cardRow}>
-          <TouchableOpacity style={homeStyle.cardContainer} onPress={handleTranslationPress}>
-            <View style={homeStyle.iconContainer}>
-              <Icon name="earth" size={24} color="#007AFF" style={homeStyle.icon} />
-            </View>
-            <Text style={homeStyle.cardText}>Traduções</Text>
-          </TouchableOpacity>
-        </View>
+          </Animatable.View>
+        </TouchableOpacity>
       </View>
+
+      <View style={homeStyle.cardRow}>
+        <TouchableOpacity onPress={handleSupportPress}>
+          <Animatable.View animation="fadeInLeft" duration={1000} style={homeStyle.cardContainer}>
+            <Icon name="question" size={32} color="#007AFF" style={homeStyle.icon} />
+            <Text style={homeStyle.cardText}>Ajuda</Text>
+          </Animatable.View>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={handleTranslationPress}>
+          <Animatable.View animation="fadeInRight" duration={1000} style={homeStyle.cardContainer}>
+            <Icon name="earth" size={32} color="#007AFF" style={homeStyle.icon} />
+            <Text style={homeStyle.cardText}>Traduções</Text>
+          </Animatable.View>
+        </TouchableOpacity>
+      </View>
+
+      <View style={homeStyle.cardRow}>
+        <TouchableOpacity onPress={handleSupportPress}>
+          <Animatable.View animation="fadeInLeft" duration={1000} style={homeStyle.cardContainer}>
+            <Icon name="question" size={32} color="#007AFF" style={homeStyle.icon} />
+            <Text style={homeStyle.cardText}>Ajuda</Text>
+          </Animatable.View>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={handleTranslationPress}>
+          <Animatable.View animation="fadeInRight" duration={1000} style={homeStyle.cardContainer}>
+            <Icon name="earth" size={32} color="#007AFF" style={homeStyle.icon} />
+            <Text style={homeStyle.cardText}>Traduções</Text>
+          </Animatable.View>
+        </TouchableOpacity>
+      </View>
+
+      <Animatable.View ref={bannerRef} style={homeStyle.bannerContainer}>
+        <Image
+          source={{ uri: 'https://helpstudent.s3.amazonaws.com/noticias/imagens/2020-03-02.jpg' }}
+          style={homeStyle.bannerImage}
+          resizeMode="cover"
+        />
+      </Animatable.View>
     </ScrollView>
   );
 };
--- src/modules/home/styles/home.style.ts
+++ src/modules/home/styles/home.style.ts
@@ -1,28 +1,29 @@
-import { StyleSheet } from 'react-native';
+import { Dimensions, StyleSheet } from 'react-native';
+const screenWidth = Dimensions.get('window').width;
+
+const cardWidth = (screenWidth - 40) / 2;
 
 const homeStyle = StyleSheet.create({
   container: {
-    flex: 1,
+    flexGrow: 1,
     padding: 16,
+    backgroundColor: '#F8F8F8',
   },
-  greetingContainer: {
+  headerContainer: {
     flexDirection: 'row',
     alignItems: 'center',
-    marginBottom: 16,
+    marginBottom: 24,
   },
   greetingText: {
     fontSize: 24,
     fontWeight: 'bold',
-  },
-  userNameContainer: {
-    borderRadius: 8,
-    paddingVertical: 4,
-    paddingHorizontal: 8,
+    marginRight: 8,
+    color: '#333',
   },
   userName: {
     fontSize: 24,
     fontWeight: 'bold',
-    color: 'blue',
+    color: '#007AFF',
   },
   cardsContainer: {
     marginBottom: 16,
@@ -33,31 +34,35 @@ const homeStyle = StyleSheet.create({
     marginBottom: 16,
   },
   cardContainer: {
-    backgroundColor: '#007AFF',
+    width: cardWidth,
+    backgroundColor: '#FFF',
     borderRadius: 8,
-    padding: 16,
+    padding: 12,
+    alignItems: 'center',
+    justifyContent: 'center',
     shadowColor: '#000',
     shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.2,
+    shadowOpacity: 0.1,
     shadowRadius: 4,
     elevation: 3,
-    alignItems: 'center',
-    width: '30%',
-  },
-  iconContainer: {
-    backgroundColor: '#FFF',
-    borderRadius: 30,
-    padding: 12,
-    marginBottom: 10,
   },
   icon: {
     alignSelf: 'center',
   },
   cardText: {
-    fontSize: 14,
-    fontWeight: 'bold',
-    color: '#FFF',
+    fontSize: 16,
+    color: '#333',
     marginTop: 8,
+  },
+  bannerContainer: {
+    height: 200,
+    borderRadius: 8,
+    overflow: 'hidden',
+    marginBottom: 16,
+  },
+  bannerImage: {
+    width: '100%',
+    height: '100%',
   },
 });
 
