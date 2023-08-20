918e3ff82d79e2571cf2375ffd0ab0df9933871a
matheusnerisxavier <matheus.nerisxa@outlook.com>
2023-08-20 14:23:27 -0300

    criando tela de detalhes dos vestibulares

--- src/modules/examsDetails/screens/ExamsDetails.tsx
+++ src/modules/examsDetails/screens/ExamsDetails.tsx
@@ -1,19 +1,39 @@
 import { format } from 'date-fns';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Image, Linking, ScrollView, Text, TouchableOpacity, View } from 'react-native';
 
 import ExamDetailsStyle from '../styles/examsDetails.style';
 
-const ExamDetails = ({ route }) => {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const ExamDetails = ({ route }: { route: any }) => {
   const { exam } = route.params;
 
+  const formatDescription = (description: string) => {
+    const paragraphs = description.split('\n\n');
+    const indentedParagraphs = paragraphs.map((paragraph) => `\t${paragraph}`);
+    return indentedParagraphs.join('\n\n');
+  };
+
   return (
-    <View style={ExamDetailsStyle.container}>
+    <ScrollView contentContainerStyle={ExamDetailsStyle.container}>
       <Text style={ExamDetailsStyle.examTitle}>{exam.title}</Text>
-      <Text style={ExamDetailsStyle.examDescription}>{exam.description}</Text>
-      <View style={ExamDetailsStyle.dateInfoContainer}>
+      <View style={ExamDetailsStyle.divider} />
+
+      {exam.image && (
+        <View style={ExamDetailsStyle.imageContainer}>
+          <View style={ExamDetailsStyle.imageWrapper}>
+            <Image source={{ uri: exam.image }} style={ExamDetailsStyle.image} />
+          </View>
+        </View>
+      )}
+
+      {(exam.image || exam.description) && <View style={ExamDetailsStyle.divider} />}
+
+      <Text style={ExamDetailsStyle.examDescription}>{formatDescription(exam.description)}</Text>
+
+      <View style={ExamDetailsStyle.infoContainer}>
         <DateInfo
-          label="Inscrição:"
+          label="Inscrições:"
           date={`${format(new Date(exam.registrationStart), 'dd/MM/yyyy')} a ${format(
             new Date(exam.registrationEnd),
             'dd/MM/yyyy',
@@ -22,27 +42,30 @@ const ExamDetails = ({ route }) => {
         <View style={ExamDetailsStyle.provaContainer}>
           <DateInfo label="Prova 1:" date={format(new Date(exam.exam1Date), 'dd/MM/yyyy')} />
           {exam.exam2Date && (
-            <DateInfo label="Prova 2:" date={format(new Date(exam.exam2Date), 'dd/MM/yyyy')} />
+            <DateInfo label="  Prova 2:" date={format(new Date(exam.exam2Date), 'dd/MM/yyyy')} />
           )}
         </View>
         {exam.resultDate && (
           <DateInfo label="Resultado:" date={format(new Date(exam.resultDate), 'dd/MM/yyyy')} />
         )}
+
+        {exam.linkEnrollment && (
+          <TouchableOpacity
+            onPress={() => Linking.openURL(exam.linkEnrollment)}
+            style={ExamDetailsStyle.linkEnrollment}
+          >
+            <Text style={ExamDetailsStyle.linkText}>Link para inscrição</Text>
+          </TouchableOpacity>
+        )}
       </View>
-    </View>
+    </ScrollView>
   );
 };
 
-const DateInfo = ({ label, date }) => (
+const DateInfo = ({ label, date }: { label: string; date: string }) => (
   <View style={ExamDetailsStyle.dateInfo}>
-    <Text>
-      <Text style={ExamDetailsStyle.dateLabelBold}>{label}</Text>
-      {label === 'Prova 1:' ? (
-        <Text style={ExamDetailsStyle.provaDate}> {date}</Text>
-      ) : (
-        <Text style={ExamDetailsStyle.prova2Date}> {date}</Text>
-      )}
-    </Text>
+    <Text style={ExamDetailsStyle.dateLabel}>{label}</Text>
+    <Text style={ExamDetailsStyle.dateText}>{date}</Text>
   </View>
 );
 
--- src/modules/examsDetails/styles/examsDetails.style.ts
+++ src/modules/examsDetails/styles/examsDetails.style.ts
@@ -6,22 +6,47 @@ const ExamDetailsStyle = StyleSheet.create({
     padding: 16,
     backgroundColor: '#FFFFFF',
   },
-  provaContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginBottom: 5,
-  },
   examTitle: {
-    fontSize: 20,
+    marginTop: 10,
+    fontSize: 24,
     fontWeight: 'bold',
     marginBottom: 10,
-  },
-  examDescription: {
-    fontSize: 16,
-    marginBottom: 20,
+    textAlign: 'center',
     color: '#333333',
   },
-  dateInfoContainer: {
+  examDescription: {
+    marginTop: 2,
+    fontSize: 16,
+    marginBottom: 20,
+    fontFamily: 'Arial',
+    color: '#666666',
+    textAlign: 'justify',
+  },
+  imageContainer: {
+    marginTop: 5,
+    alignItems: 'center',
+    marginBottom: 5,
+    borderBottomWidth: 1,
+    borderBottomColor: '#DDDDDD',
+  },
+  imageWrapper: {
+    width: '100%',
+    height: 200,
+    borderRadius: 10,
+    overflow: 'hidden',
+    backgroundColor: '#FFFFFF',
+    shadowColor: '#000',
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+    shadowOffset: { width: 0, height: 2 },
+    elevation: 5,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
+  },
+  infoContainer: {
     marginBottom: 20,
   },
   dateInfo: {
@@ -29,15 +54,38 @@ const ExamDetailsStyle = StyleSheet.create({
     alignItems: 'center',
     marginBottom: 10,
   },
-  dateLabelBold: {
+  dateLabel: {
+    fontSize: 16,
     fontWeight: 'bold',
-    marginRight: 5,
+    marginRight: 10,
+    color: '#333333',
   },
-  provaDate: {
+  dateText: {
+    fontSize: 16,
+    color: '#666666',
+  },
+  provaContainer: {
+    flexDirection: 'row',
+    marginBottom: 2,
+  },
+  linkEnrollment: {
+    fontSize: 16,
+    color: '#007AFF',
+    textDecorationLine: 'underline',
+    marginTop: 2,
+  },
+  linkText: {
+    fontWeight: 'bold',
     color: '#007AFF',
   },
-  prova2Date: {
-    color: '#E17055',
+  link: {
+    color: '#007AFF',
+    textDecorationLine: 'underline',
+  },
+  divider: {
+    height: 1,
+    backgroundColor: '#DDDDDD',
+    marginVertical: 20,
   },
 });
 
