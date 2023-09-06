42726369940b85c912dc56ebdde0a6bd790f7e26
matheusnerisxavier <matheus.nerisxa@outlook.com>
2023-09-05 21:45:11 -0300

    malhorias

--- src/modules/login/screens/Login.tsx
+++ src/modules/login/screens/Login.tsx
@@ -1,3 +1,4 @@
+import { useNavigation } from '@react-navigation/native';
 import React, { useEffect, useState } from 'react';
 import { Animated, TouchableOpacity, View } from 'react-native';
 import { KeyboardAvoidingView } from 'react-native';
@@ -11,6 +12,8 @@ import { useLogin } from '../hooks/useLogin';
 import { ContainerLogin, Imagelogo } from '../styles/login.style';
 
 const Login = () => {
+  const navigation = useNavigation();
+
   const {
     email,
     password,
@@ -19,7 +22,6 @@ const Login = () => {
     handleOnPress,
     handleOnChangeEmail,
     handleOnChangePassword,
-    handleGoToCreateUser,
   } = useLogin();
 
   const [animationCompleted, setAnimationCompleted] = useState(false);
@@ -101,31 +103,65 @@ const Login = () => {
               onChange={handleOnChangePassword}
             />
           </Animated.View>
-          <TouchableOpacity onPress={handleGoToCreateUser}>
-            <Animated.View
-              // eslint-disable-next-line react-native/no-inline-styles
-              style={{
-                opacity: animation,
-                transform: [
-                  {
-                    translateY: animation.interpolate({
-                      inputRange: [0, 1],
-                      outputRange: [50, 0],
-                    }),
-                  },
-                ],
-                width: '100%',
-              }}
-            >
-              <Text
-                margin="16px"
-                type={textTypes.PARAGRAPH_SEMI_BOLD}
-                color={theme.colors.blueTheme.blue80}
+
+          <View
+            // eslint-disable-next-line react-native/no-inline-styles
+            style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}
+          >
+            <TouchableOpacity onPress={() => navigation.navigate('CreateUser')}>
+              <Animated.View
+                // eslint-disable-next-line react-native/no-inline-styles
+                style={{
+                  opacity: animation,
+                  transform: [
+                    {
+                      translateY: animation.interpolate({
+                        inputRange: [0, 1],
+                        outputRange: [50, 0],
+                      }),
+                    },
+                  ],
+                  width: 'auto',
+                  marginTop: 20,
+                }}
               >
-                Cadastrar usuário
-              </Text>
-            </Animated.View>
-          </TouchableOpacity>
+                <Text margin="5px" type={textTypes.PARAGRAPH_SEMI_BOLD} color="#253494">
+                  PRIMEIRO ACESSO
+                </Text>
+              </Animated.View>
+            </TouchableOpacity>
+
+            <Text
+              // eslint-disable-next-line react-native/no-inline-styles
+              style={{ marginHorizontal: 10, color: theme.colors.blueTheme.blue80, marginTop: 20 }}
+            >
+              |
+            </Text>
+
+            <TouchableOpacity onPress={() => navigation.navigate('RecuperarSenha')}>
+              <Animated.View
+                // eslint-disable-next-line react-native/no-inline-styles
+                style={{
+                  opacity: animation,
+                  transform: [
+                    {
+                      translateY: animation.interpolate({
+                        inputRange: [0, 1],
+                        outputRange: [50, 0],
+                      }),
+                    },
+                  ],
+                  width: 'auto',
+                  marginTop: 20,
+                }}
+              >
+                <Text margin="5px" type={textTypes.PARAGRAPH_SEMI_BOLD} color="#253494">
+                  RECUPERAR SENHA
+                </Text>
+              </Animated.View>
+            </TouchableOpacity>
+          </View>
+
           <Animated.View
             // eslint-disable-next-line react-native/no-inline-styles
             style={{
@@ -139,6 +175,7 @@ const Login = () => {
                 },
               ],
               width: '100%',
+              marginTop: 20,
             }}
           >
             <Button
